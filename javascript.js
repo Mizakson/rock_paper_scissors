c8953501b2647d2b34ee2c8a86a785bc0d8f2e7c
Mizakson <maxwellizakson@gmail.com>
2023-08-09 11:26:23 -0400

    Create getComputerChoice function
    add comment with explanation

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// rock paper scissors program
+
+
+/*
+getComputerChoice function
+
+array of possible choices - rock, paper scissors
+
+get a random number using Math.floor and Math.random from 0-2
+number is from 0 to 2 because array indexing starts at 0
+
+computerChoice is a random element of the rpsArray
+*/
+
+function getComputerChoice() {
+    const rpsArray = ['Rock', 'Paper', 'Scissors'];
+    const randomInt = Math.floor(Math.random() * 3);
+    const computerChoice = rpsArray[randomInt];
+    return computerChoice;
+}
